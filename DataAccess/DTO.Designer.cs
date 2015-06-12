0eb96d8c8d0dae6b6d2dec33100facd89c2a1879
Regu2 <reinguet@wb.de>
2015-06-12 11:11:54 +0200

    Komponenten-Klasse eingefügt

---
+++ DataAccess/DTO.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace DataAccess
+{
+    partial class DTO
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oleDbConnection1 = new System.Data.OleDb.OleDbConnection();
+            // 
+            // oleDbConnection1
+            // 
+            this.oleDbConnection1.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\\regu\\csharp\\KundenManagement\\Data" +
+    "Access\\Data\\kunden.mdb";
+
+        }
+
+        #endregion
+
+        private System.Data.OleDb.OleDbConnection oleDbConnection1;
+    }
+}
---
+++ DataAccess/DTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public partial class DTO : Component
+    {
+        public DTO()
+        {
+            InitializeComponent();
+        }
+
+        public DTO(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
